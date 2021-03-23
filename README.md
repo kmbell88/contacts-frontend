5c3cc4a74b39d33f8fd9784831d8cbf3102b1d3f
kmbell88 <kmbell88@gmail.com>
2021-03-22 22:28:23 -0700

    Set up contacts component

--- README.md
+++ README.md
@@ -1,10 +1,8 @@
 # Contacts App
 
-
 This is the Angular frontend of my contacts full-stack application, using an express backend.
-
-
 
 Change log:
 
+- 22 March 2021: Set up contacts component with http module. Receives contacts from backend via get request and puts them in a table. No styling yet.
 - 20 March 2021: Initializing the Angular project and repository.
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactsComponent } from './contacts/contacts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
